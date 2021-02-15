0b04f44aed5c69a9acb59a114c276b76fae5f339
brunolimadev <bruno.lima.dev@outlook.com>
2021-02-15 16:30:35 -0300

    chore: Organize functions

--- js/script.js
+++ js/script.js
@@ -4,25 +4,19 @@
     let headerElement = document.querySelector("header");
     let buttonElement = document.querySelector(".btn");
     let cardsContainerElement = document.querySelector(".our-creations__cards");
+    let ourCreationsTopElement = document.querySelector(".our-creations__top");
+    let footerTopContainer = document.querySelector(".footer-top");
+    let footerMiddleContainer = document.querySelector(".footer-middle");
     let socialMediaContainerElement = document.querySelector(
         ".footer-top__social-media"
     );
-    let ourCreationsTopElement = document.querySelector(".our-creations__top");
 
-    let footerTopContainer = document.querySelector(".footer-top");
-    let footerMiddleContainer = document.querySelector(".footer-middle");
+    handleDetecScreenAndAjustElements();
+    handleResizeDocument();
+    handleClickOnHeader();
 
-    if (window.screen.width <= 768) {
-        buttonElement.classList.add("--lg");
-        cardsContainerElement.appendChild(buttonElement);
-        footerMiddleContainer
-            .querySelector("nav")
-            .insertAdjacentElement("beforeend", socialMediaContainerElement);
-        socialMediaContainerElement.style.display = "block";
-    }
-
-    window.addEventListener("resize", () => {
-        if (window.screen.width <= 768) {
+    function handleDetecScreenAndAjustElements() {
+        if (window.screen.width <= 768 || window.outerWidth <= 768) {
             buttonElement.classList.add("--lg");
             cardsContainerElement.appendChild(buttonElement);
             footerMiddleContainer
@@ -33,27 +27,45 @@
                 );
             socialMediaContainerElement.style.display = "block";
         }
-        if (window.screen.width > 768) {
-            buttonElement.classList.contains("--lg") &&
-                buttonElement.classList.remove("--lg");
-            ourCreationsTopElement.appendChild(buttonElement);
-            footerTopContainer.appendChild(socialMediaContainerElement);
-        }
-    });
+    }
 
-    headerElement.addEventListener("click", (event) => {
-        let target = event.target;
+    function handleResizeDocument() {
+        window.addEventListener("resize", () => {
+            if (window.screen.width <= 768 || window.outerWidth <= 768) {
+                buttonElement.classList.add("--lg");
+                cardsContainerElement.appendChild(buttonElement);
+                footerMiddleContainer
+                    .querySelector("nav")
+                    .insertAdjacentElement(
+                        "beforeend",
+                        socialMediaContainerElement
+                    );
+                socialMediaContainerElement.style.display = "block";
+            }
+            if (window.screen.width > 768) {
+                buttonElement.classList.contains("--lg") &&
+                    buttonElement.classList.remove("--lg");
+                ourCreationsTopElement.appendChild(buttonElement);
+                footerTopContainer.appendChild(socialMediaContainerElement);
+            }
+        });
+    }
 
-        if (
-            target.classList[0] === "hamburguer-menu" ||
-            target.classList[0] === "hamburguer-menu__line"
-        ) {
-            mobileMenuElement.classList.toggle("d-none");
-            mobileMenuElement.classList.toggle("mobile-menu--open");
-            hamburguerMenuElement.classList.toggle("open");
-            mobileMenuElement.classList.contains("d-none")
-                ? (document.body.style.overflow = "auto")
-                : (document.body.style.overflow = "hidden");
-        }
-    });
+    function handleClickOnHeader() {
+        headerElement.addEventListener("click", (event) => {
+            let target = event.target;
+
+            if (
+                target.classList[0] === "hamburguer-menu" ||
+                target.classList[0] === "hamburguer-menu__line"
+            ) {
+                mobileMenuElement.classList.toggle("d-none");
+                mobileMenuElement.classList.toggle("mobile-menu--open");
+                hamburguerMenuElement.classList.toggle("open");
+                mobileMenuElement.classList.contains("d-none")
+                    ? (document.body.style.overflow = "auto")
+                    : (document.body.style.overflow = "hidden");
+            }
+        });
+    }
 })();
